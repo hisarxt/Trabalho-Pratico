3b10610f4326dcc97acb31c3e96bbeab13c768ae
Arthur Fernandes Alencar <123188031+hisarxt@users.noreply.github.com>
2024-08-10 12:29:53 -0300

    Update Turma.java
    app.Turma atualizado

--- trabalhoPratico/src/app/Turma.java
+++ trabalhoPratico/src/app/Turma.java
@@ -10,4 +10,55 @@ public class Turma {
     ArrayList<Aluno> alunosMatriculados = new ArrayList<Aluno>();
     Sala sala;
     String horario;
+    
+    public Turma(String nome, String codigoDaTurma, Disciplina disciplina, Professor professor) {
+    	this.nome = nome;
+    	this.codigoDaTurma = codigoDaTurma;
+    	this.disciplina = disciplina;
+    	this.professor = professor;
+    }
+    
+    public Turma(String codigoDaTurma, Professor professor, Disciplina disciplina) {
+    	
+	}
+
+	public String getNome() {
+    	return nome;
+    }
+    
+    public String getCodigoDaTurma() {
+    	return codigoDaTurma;
+    }
+    
+    public Disciplina getDisciplina() {
+    	return disciplina;
+    }
+    
+    public Professor getProfessor() {
+    	return professor;
+    }
+    
+    public void matricularAluno(Aluno aluno) {
+    	alunosMatriculados.add(aluno);
+    }
+    
+    public void imprimirListaDePresenca() {
+    	System.out.println("Lista de Presenca: ");
+    	System.out.println("Disciplina: " + disciplina.getNome());
+    	System.out.println("Professor: " + professor.getNome());
+    	System.out.println("Codigo da turma: " + codigoDaTurma);
+    	System.out.println("Alunos: ");
+    	for(Aluno aluno : alunosMatriculados) {
+    		System.out.println(aluno.getMatricula() + " - " + aluno.getNome());
+    	}
+    }
+    
+    public String toString() {
+    	return "Turma{" +
+                "codigo='" + codigoDaTurma + '\'' +
+                ", professor=" + professor +
+                ", disciplina=" + disciplina +
+                ", alunos=" + alunosMatriculados +
+                '}';
+    }
 }
