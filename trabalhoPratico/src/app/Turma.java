5898c876fb74badf475f7680521f5bc9f6136125
Arthur Fernandes Alencar <123188031+hisarxt@users.noreply.github.com>
2024-08-12 13:21:47 -0300

    Atualizado analise do codigo Turma.java

--- trabalhoPratico/src/app/Turma.java
+++ trabalhoPratico/src/app/Turma.java
@@ -1,64 +1,103 @@
-package app;
+package cadastros;
+
+import app.Aluno;
+import app.Disciplina;
+import app.Professor;
+import app.Turma;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Turma {
-    String nome;
-    String codigoDaTurma;
-    Disciplina disciplina;
-    Professor professor;
-    ArrayList<Aluno> alunosMatriculados = new ArrayList<Aluno>();
-    Sala sala;
-    String horario;
-    
-    public Turma(String nome, String codigoDaTurma, Disciplina disciplina, Professor professor) {
-    	this.nome = nome;
-    	this.codigoDaTurma = codigoDaTurma;
-    	this.disciplina = disciplina;
-    	this.professor = professor;
-    }
-    
-    public Turma(String codigoDaTurma, Professor professor, Disciplina disciplina) {
-    	
+	public class CadastroTurmas {
+		private List<Turma> turmas;
+		private CadastroProfessores cadastroProfessor;
+		private CadastroDisciplinas cadastroDisciplina;
+		private CadastroAlunos cadastroAluno;
+	
+	public CadastroTurmas(CadastroProfessores cadastroProfessor, CadastroDisciplinas cadastroDisciplina){
+		this.turmas = new ArrayList<>();
+		this.cadastroProfessor = cadastroProfessor;
+		this.cadastroDisciplina = cadastroDisciplina;
+	}
+	public void cadastrarTurma(String nome, String codigoDaTurma, String matriculaFubProfessor) {
+		Professor professor = cadastroProfessor.pesquisarProfessores(matriculaFubProfessor);
+		Disciplina disciplina = cadastroDisciplina.pesquisarDisciplina(nome);	
+	
+		if(professor == null) {
+			System.out.println("Erro: Professor com matricula " + matriculaFubProfessor + "nao encontrado");
+			return;
+		}
+		if(disciplina == null) {
+			System.out.println("Erro: Disciplina com codigo " + codigoDaTurma + "nao encontrada");
+			return;
+		}
+	
+		Turma turma = new Turma(codigoDaTurma, professor, disciplina);
+		turmas.add(turma);
+		System.out.println("Turma cadastrada com sucesso: " + codigoDaTurma);
 	}
 
-	public String getNome() {
-    	return nome;
+	public Turma pesquisarTurma(String codigoDaTurma) {
+        for (Turma turma : turmas) {
+            if (turma.getCodigoDaTurma().equalsIgnoreCase(codigoDaTurma)) {
+                return turma;
+            }
+        }
+        return null;
     }
-    
-    public String getCodigoDaTurma() {
-    	return codigoDaTurma;
+	
+	public void atualizarTurma(String codigoDaTurma, String novaMatriculaFubProfessor, String novaDisciplinaCodigo) {
+		Turma turma = pesquisarTurma(codigoDaTurma);
+		if(turma != null) {
+			Professor novoProfessor = cadastroProfessor.pesquisarProfessores(novaMatriculaFubProfessor);
+			Disciplina novaDisciplina = cadastroDisciplina.pesquisarDisciplina(novaDisciplinaCodigo);
+			turma = new Turma(codigoDaTurma, novoProfessor, novaDisciplina);
+		}
+	}
+	
+	public boolean removerTurma(String codigoDaTurma) {
+        Turma turma = pesquisarTurma(codigoDaTurma);
+        if (turma != null) {
+            turmas.remove(turma);
+            return true;
+        }
+        return false;
     }
-    
-    public Disciplina getDisciplina() {
-    	return disciplina;
+	
+	public void matricularAlunoNaTurma(String codigoDaTurma, String matriculaAluno) {
+		Turma turma = pesquisarTurma(codigoDaTurma);
+		Aluno aluno = cadastroAluno.pesquisarAlunos(matriculaAluno);
+		
+		if(turma != null && aluno != null) {
+			turma.matricularAluno(aluno);
+		} else {
+			System.out.println("Turma ou aluno nao encontrado.");
+		}
+	}
+
+
+	public void listarTurmas() {
+        System.out.println("Lista de turmas:");
+        for (Turma t : turmas) {
+            System.out.println(t.getCodigoDaTurma() + ": " + t.getProfessor().getNome() + " - " + t.getDisciplina().getNome());
+        }
     }
-    
-    public Professor getProfessor() {
-    	return professor;
-    }
-    
-    public void matricularAluno(Aluno aluno) {
-    	alunosMatriculados.add(aluno);
-    }
-    
-    public void imprimirListaDePresenca() {
-    	System.out.println("Lista de Presenca: ");
-    	System.out.println("Disciplina: " + disciplina.getNome());
-    	System.out.println("Professor: " + professor.getNome());
-    	System.out.println("Codigo da turma: " + codigoDaTurma);
-    	System.out.println("Alunos: ");
-    	for(Aluno aluno : alunosMatriculados) {
-    		System.out.println(aluno.getMatricula() + " - " + aluno.getNome());
-    	}
-    }
-    
-    public String toString() {
-    	return "Turma{" +
-                "codigo='" + codigoDaTurma + '\'' +
-                ", professor=" + professor +
-                ", disciplina=" + disciplina +
-                ", alunos=" + alunosMatriculados +
-                '}';
-    }
+	
+	public void imprimirListaDePresenca(String codigoDaTurma) {
+		Turma turma = pesquisarTurma(codigoDaTurma);
+		if(turma != null) {
+			turma.imprimirListaDePresenca();
+		} else {
+			System.out.println("Turma nao encontrada");
+		}
+	}
+	public void atualizarTurma(String codigoDaTurma, Turma atualizacao) {
+		
+	}
+	public void cadastrarTurma(Turma novaTurma) {
+		
+	}
+	public void removerTurma(Turma pesquisa) {
+		
+	}
 }
